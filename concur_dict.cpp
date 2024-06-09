2c7b84e2d917a14b87177d45f82887a7f5894016
Yuvraj Virdi <yuvraj.virdi@live.com>
2024-06-09 14:50:59 -0400

    Update concur_dict.cpp

--- concur_dict.cpp
+++ concur_dict.cpp
@@ -1,22 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 #include <mutex>
 #include <shared_mutex>
 #include <string>
 #include <functional>
-#include <iostream>
-#include <unordered_map>
-#include <vector>
+#include "file_io.h" 
 
 using namespace std;
 
 template <typename Key, typename Value, size_t NumBuckets = 16>
 class ConcurrentDictionary {
 public:
-    ConcurrentDictionary() : buckets(NumBuckets) {}
+    ConcurrentDictionary(const string& filename) : buckets(NumBuckets), filename(filename) {}
 
     void insert(const Key& key, const Value& value) {
         size_t bucket = get_bucket(key);
         unique_lock<mutex> lock(buckets[bucket].mutex);
         buckets[bucket].map[key] = value;
+        write_to_disk(filename.c_str(), key.c_str(), value.c_str());
     }
 
     bool remove(const Key& key) {
@@ -26,6 +28,7 @@ public:
         auto it = bucket_map.find(key);
         if (it != bucket_map.end()) {
             bucket_map.erase(it);
+            // TODO: disk removal
             return true;
         }
         return false;
@@ -39,6 +42,13 @@ public:
         if (it != bucket_map.end()) {
             value = it->second;
             return true;
+        } else {
+            char c_value[256];
+            read_from_disk(filename.c_str(), key.c_str(), c_value, sizeof(c_value));
+            if (strlen(c_value) > 0) {
+                value = string(c_value);
+                return true;
+            }
         }
         return false;
     }
@@ -57,9 +67,26 @@ private:
     };
 
     vector<Bucket> buckets;
+    string filename;
 
     size_t get_bucket(const Key& key) const {
         static hash<Key> hasher;
         return hasher(key) % buckets.size();
     }
 };
+
+int main() {
+    ConcurrentDictionary<string, string> dict("data.txt");
+
+    dict.insert("key1", "value1");
+    dict.insert("key2", "value2");
+
+    string value;
+    if (dict.get("key1", value)) {
+        cout << "key1: " << value << endl;
+    } else {
+        cout << "key1 not found" << endl;
+    }
+
+    return 0;
+}
